734ca3208f54354d3da9be588b467bd4f147db9f
ZalaNidhish <153850082+ZalaNidhish@users.noreply.github.com>
2025-03-02 00:19:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 
 app.get('/', function(req, res) {
-        res.render("index");    
+        res.render(__dirname + "index");    
     })
 
 app.get('/register', function (req,res) {
